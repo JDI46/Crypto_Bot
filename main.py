24df7b8c195d07c35dee679104b03e8ff4c3cd04
Xander <xwhitby20@gmail.com>
2024-10-24 13:09:28 -0600

    made minor object changes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+###after I figure out the search words class, take line 6, 7, 8 and make a different python then import the driver as a module
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 #calls driver to start working
@@ -17,10 +21,11 @@ class Driver:
         else:
             raise ValueError("Driver doesn't work")
 
-    def get_title(self):
-        self.driver.get('https://www.youtube.com/')
+    def get_website(self, url):
+        self.driver.get(url)
         time.sleep(10)
         return self.driver
+    
 
     def quit_driver(self):
         return self.driver.quit()
@@ -33,7 +38,7 @@ class SearchWeb:
 
     #pass self to it, then take in a search button object, string
     def activate_search_button(self):
-        pass
+        pass     
     
     #takes in search word parameters after activating previous function
     #also needs to have a sleep time in order to have enough time to find the words
@@ -41,10 +46,10 @@ class SearchWeb:
         pass
 
 
-
 driver = Driver()
-title = driver.get_title()
+url = "https://www.youtube.com/"
+title = driver.get_title(url)
 print(title)
 
 
-find_button = driver.find_eleme
+
