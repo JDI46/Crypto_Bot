8b9b5b022afb400ec98ec5535828887979fe4c8f
Xander <xwhitby20@gmail.com>
2024-10-24 11:55:57 -0600

    Got driver to return title with a class

--- main.py
+++ main.py
@@ -5,10 +5,27 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 #calls driver to start working
-driver = webdriver.Chrome()
-url = 'https://www.youtube.com/'
 
-if driver:
-    driver.get(url)
-    title = driver.title
-    print(title)
+
+class Driver:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def start_driver(self):
+        if self.driver:
+            return self.driver
+        else:
+            raise ValueError("Driver doesn't work")
+
+    def get_title(self):
+        self.driver.get('https://www.youtube.com/')
+        title = self.driver.title
+        print(title)
+
+    def quit_driver(self):
+        return self.driver.quit()
+
+
+driver = Driver()
+title = driver.get_title()
+print(title)
