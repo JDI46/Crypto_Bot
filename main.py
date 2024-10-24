93d9276faa763faf691ff58339bb75604dcb5972
Xander <xwhitby20@gmail.com>
2024-10-24 10:58:01 -0600

    figured out if statement with webdriver

--- main.py
+++ main.py
@@ -5,47 +5,10 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 #calls driver to start working
-class Driver:
-    def __init__(self):
-        self.driver = None
+driver = webdriver.Chrome()
+url = 'https://www.youtube.com/'
 
-    def start_driver(self, driver="chrome"):
-        if not self.driver:
-            if self.driver == "chrome":
-                self.driver == webdriver.Chrome()
-            else:
-                raise ValueError("Invalid Browser")
-
-    def stop_driver(self):
-        if self.driver:
-            self.driver.quit()
-            self.driver = None
-    
-    def navigate_url(self, url):
-        if not self.driver:
-            return
-        self.driver.get(url)
-
-    def get_title(self):
-        if not self.driver:
-            return None
-        return self.driver.title
-    
-
-Driver()
-#needs to get website name
-
-
-
-class GetHTML:
-    video_data = {}
-    def __init__(self, video_html):
-        pass
-
-    def title_html(self):
-        pass
-
-    def looped_data():
-        pass
-
-
+if driver:
+    driver.get(url)
+    title = driver.title
+    print(title)
