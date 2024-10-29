b37501a8d0826b4279839716cff6f34e5feadd5f
Xander <xwhitby20@gmail.com>
2024-10-28 19:57:21 -0600

    made html class to bring elements to the bot

---
+++ crypto_bot.py
@@ -0,0 +1,20 @@
+from driver_module import Driver
+import asyncio
+
+
+class Bot:
+    def __init__(self):
+        self.start_driver = Driver()
+    
+
+    def trend_1(self):
+        pass
+
+    def trend_2(self):
+        pass
+
+    def trend_3(self):
+        pass
+
+def calculate_trends():
+    pass
