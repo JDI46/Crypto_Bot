2e702a1a1088e1a0892b1b2a58693c7213d5863d
Xander <xwhitby20@gmail.com>
2024-10-29 08:39:09 -0600

    took away class to add async

--- crypto_bot.py
+++ crypto_bot.py
@@ -1,14 +1,23 @@
 from driver_module import Driver
 import asyncio
 
+class HTML:
+    def __init__():
+        pass
+
+    def base_html():
+        pass
+
 
 class Bot:
+    data = {}
     def __init__(self):
         self.start_driver = Driver()
     
 
     def trend_1(self):
-        pass
+        for i in self.start_driver():
+
 
     def trend_2(self):
         pass
