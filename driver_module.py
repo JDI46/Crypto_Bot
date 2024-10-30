839716efeaa8add99ba0157662dcff14964983cb
Xander <xwhitby20@gmail.com>
2024-10-30 17:37:15 -0600

    trying to search for html using selenium

--- driver_module.py
+++ driver_module.py
@@ -4,45 +4,35 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 import time
 
 
 #maybe i need to add object functions to the whole driver then access the methods in a different module, then mutli thread it or async
 
 
-# if driver:
-#     print(driver)
-# else:
-#     raise ValueError("Driver doesn't work")
-
-
-#         #this is hardcoded due to me getting errors from creating and element on the get title call at the bottom
-#         #I only want to use youtube for my research question
-# try:
-#     time.sleep(10)
-# except:
-#     raise Exception("Sleep didn't work")
-
             
-
-    
-
 def web_driver(web, driver):
         while driver:
-            time.sleep(10)
+            time.sleep(20)
             driver.quit()
             break
 
         return web, driver
 
-    
-        
-            
+
 def website_html():
-    pass
+    while web_driver(website, driver):
+        element = driver.find_element(By.NAME, "search")
+        for i in element:
+            i = element.send_keys("google")
+            break
+        return i, element.click()
+            
+
 
 
 
@@ -61,5 +51,6 @@ class Update:
 
 # soup = BeautifulSoup(html_doc, 'html.parser')
 driver = webdriver.Chrome()
-website = driver.get("https://coinmarketcap.com/")
-web_driver(website, driver)
+website = driver.get("https://www.google.com/")
+
+website_html()
